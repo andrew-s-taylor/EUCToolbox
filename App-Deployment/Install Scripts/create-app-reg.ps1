99e79065c5624e6e442208b585f8c6341490dd60
andrew-s-taylor <andrew@tinymice.co.uk>
2024-08-12 11:47:58 +0100

    Deployment scripts

---
+++ App-Deployment/Install Scripts/create-app-reg.ps1
@@ -0,0 +1,201 @@
+##Creates a multi-tenant App Reg
+##Secret is randomly generated
+##App ID and Secret passed to the output
+
+#Install MS Graph if not available
+if (Get-Module -ListAvailable -Name Microsoft.Graph.Authentication) {
+    write-output "Microsoft Graph Authentication Already Installed"
+} 
+else {
+        Install-Module -Name Microsoft.Graph.Authentication -Scope CurrentUser -Repository PSGallery -Force
+        write-output "Microsoft Graph Authentication Installed"
+}
+
+#Install MS Graph if not available
+if (Get-Module -ListAvailable -Name Microsoft.Graph.Applications) {
+    write-output "Microsoft Graph Applications Already Installed"
+} 
+else {
+        Install-Module -Name Microsoft.Graph.Applications -Scope CurrentUser -Repository PSGallery -Force
+        write-output "Microsoft Graph Applications Installed"
+}
+
+#Import Module
+Import-Module Microsoft.Graph.Authentication
+Import-Module Microsoft.Graph.Applications
+Function Connect-ToGraph {
+    <#
+.SYNOPSIS
+Authenticates to the Graph API via the Microsoft.Graph.Authentication module.
+ 
+.DESCRIPTION
+The Connect-ToGraph cmdlet is a wrapper cmdlet that helps authenticate to the Intune Graph API using the Microsoft.Graph.Authentication module. It leverages an Azure AD app ID and app secret for authentication or user-based auth.
+ 
+.PARAMETER Tenant
+Specifies the tenant (e.g. contoso.onmicrosoft.com) to which to authenticate.
+ 
+.PARAMETER AppId
+Specifies the Azure AD app ID (GUID) for the application that will be used to authenticate.
+ 
+.PARAMETER AppSecret
+Specifies the Azure AD app secret corresponding to the app ID that will be used to authenticate.
+
+.PARAMETER Scopes
+Specifies the user scopes for interactive authentication.
+ 
+.EXAMPLE
+Connect-ToGraph -TenantId $tenantID -AppId $app -AppSecret $secret
+ 
+-#>
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory = $false)] [string]$Tenant,
+        [Parameter(Mandatory = $false)] [string]$AppId,
+        [Parameter(Mandatory = $false)] [string]$AppSecret,
+        [Parameter(Mandatory = $false)] [string]$scopes
+    )
+
+    Process {
+        Import-Module Microsoft.Graph.Authentication
+        $version = (get-module microsoft.graph.authentication | Select-Object -expandproperty Version).major
+
+        if ($AppId -ne "") {
+            $body = @{
+                grant_type    = "client_credentials";
+                client_id     = $AppId;
+                client_secret = $AppSecret;
+                scope         = "https://graph.microsoft.com/.default";
+            }
+     
+            $response = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$Tenant/oauth2/v2.0/token -Body $body
+            $accessToken = $response.access_token
+     
+            $accessToken
+            if ($version -eq 2) {
+                write-host "Version 2 module detected"
+                $accesstokenfinal = ConvertTo-SecureString -String $accessToken -AsPlainText -Force
+            }
+            else {
+                write-host "Version 1 Module Detected"
+                Select-MgProfile -Name Beta
+                $accesstokenfinal = $accessToken
+            }
+            $graph = Connect-MgGraph  -AccessToken $accesstokenfinal 
+            Write-Host "Connected to Intune tenant $TenantId using app-based authentication (Azure AD authentication not supported)"
+        }
+        else {
+            if ($version -eq 2) {
+                write-host "Version 2 module detected"
+            }
+            else {
+                write-host "Version 1 Module Detected"
+                Select-MgProfile -Name Beta
+            }
+            $graph = Connect-MgGraph -scopes $scopes
+            Write-Host "Connected to Intune tenant $($graph.TenantId)"
+        }
+    }
+}    
+
+Connect-ToGraph -Scopes "Application.Read.All,Application.ReadWrite.All,User.Read.All"
+
+
+function Get-RandomPassword {
+    param (
+        [Parameter(Mandatory)]
+        [int] $length,
+        [int] $amountOfNonAlphanumeric = 1
+    )
+    Add-Type -AssemblyName 'System.Web'
+    return [System.Web.Security.Membership]::GeneratePassword($length, $amountOfNonAlphanumeric)
+}
+
+###############################################################################
+#Create AAD Application
+###############################################################################
+$AppName =  "AppDeployEUCToolbox"
+$App = New-MgApplication -DisplayName $AppName -SignInAudience AzureADMultipleOrgs
+$APPObjectID = $App.Id
+
+###############################################################################
+#Add a ClientSecret
+###############################################################################
+$passwordCred = @{
+    "displayName" = "AppDeploySecret"
+    "endDateTime" = (Get-Date).AddMonths(+24)
+}
+$ClientSecret2 = Add-MgApplicationPassword -ApplicationId $APPObjectID -PasswordCredential $passwordCred
+
+$appsecret = $ClientSecret2.SecretText
+
+###############################################################################
+#Add Permissions
+###############################################################################
+#Add Delegated Permission
+$params = @{
+    RequiredResourceAccess = @(
+        @{
+            ResourceAppId = "00000003-0000-0000-c000-000000000000"
+            ResourceAccess = @(
+                @{
+                    Id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+                    Type = "Scope"
+                },
+                @{
+                    Id = "78145de6-330d-4800-a6ce-494ff2d33d07"
+                    Type = "Role"
+                },            
+                @{
+                    Id = "9241abd9-d0e6-425a-bd4f-47ba86e767a4"
+                    Type = "Role"
+                },
+                @{
+                    Id = "62a82d76-70ea-41e2-9197-370581804d09"
+                    Type = "Role"
+                },
+                @{
+                    Id = "dbaae8cf-10b5-4b86-a4a1-f871c94c6695"
+                    Type = "Role"
+                },
+                @{
+                    Id = "498476ce-e0fe-48b0-b801-37ba7e2685c6"
+                    Type = "Role"
+                }
+            )
+        }
+    )
+}
+Update-MgApplication -ApplicationId $APPObjectID -BodyParameter $params
+
+###############################################################################
+#Redirect URI
+#If you need to add Redirect URI's.
+###############################################################################
+#Redirect URI
+$App = Get-MgApplication -ApplicationId $APPObjectID -Property *
+$AppId = $App.AppId
+$RedirectURI = @()
+$RedirectURI += "https://login.microsoftonline.com/common/oauth2/nativeclient"
+$RedirectURI += "msal" + $AppId + "://auth"
+$RedirectURI += "https://intunebackup.com"
+
+$params = @{
+    RedirectUris = @($RedirectURI)
+}
+Update-MgApplication -ApplicationId $APPObjectID -IsFallbackPublicClient -PublicClient $params
+
+###############################################################################
+#Grant Admin Consent - Opens URL in Browser
+###############################################################################
+#https://login.microsoftonline.com/{tenant-id}/adminconsent?client_id={client-id}
+$App = Get-MgApplication | Where-Object {$_.DisplayName -eq $AppName}
+$TenantID = $App.PublisherDomain
+$AppID = $App.AppID
+$URL = "https://login.microsoftonline.com/$TenantID/adminconsent?client_id=$AppID"
+Start-Process $URL
+
+write-host "Your App ID is $AppID"
+
+write-host "Your App Secret is $appsecret"
+
